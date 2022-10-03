7e02baf2d52373cf08d995d8d1c776127c6f6bde
Giorgos Pavloglou <georpavloglou@gmail.com>
2022-10-03 17:12:39 +0300

    SECTION-4: initialize starter project for spring boot

---
+++ section-4-learn-spring-boot/src/main/java/com/in28minutes/springboot/section4learnspringboot/Section4LearnSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.section4learnspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section4LearnSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section4LearnSpringBootApplication.class, args);
+	}
+
+}
---
+++ section-4-learn-spring-boot/src/test/java/com/in28minutes/springboot/section4learnspringboot/Section4LearnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.section4learnspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section4LearnSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
