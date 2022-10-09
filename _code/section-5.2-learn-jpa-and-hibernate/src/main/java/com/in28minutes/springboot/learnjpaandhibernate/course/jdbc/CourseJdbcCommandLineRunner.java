63d3d46f69ead2456b120d75e610692892e8086a
Giorgos Pavloglou <georpavloglou@gmail.com>
2022-10-09 14:02:56 +0300

    create jdbc query and refactor Course.java using @lombok

--- _code/section-5.2-learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ _code/section-5.2-learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -11,12 +11,16 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
     @Autowired
     private CourseJdbcRepository repository;
 
+    private static final String AUTHOR= "in28minutes";
+
     @Override
     public void run(String... args) throws Exception {
-        repository.insert(new Course(1, "Learn AWS", "in28minutes"));
-        repository.insert(new Course(2, "Learn Azure Now!", "in28minutes"));
-        repository.insert(new Course(3, "Learn Spring Boot", "in28minutes"));
+        repository.insert(new Course(1, "Learn AWS", AUTHOR));
+        repository.insert(new Course(2, "Learn Azure Now!", AUTHOR));
+        repository.insert(new Course(3, "Learn Spring Boot", AUTHOR));
 
         repository.deleteById(1);
+        System.out.println(repository.findById(2));
+        System.out.println(repository.findById(3));
     }
 }
