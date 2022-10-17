9f5c93083eb1a91e6d2b78f6cd6b9496ce234833
Giorgos Pavloglou <georpavloglou@gmail.com>
2022-10-18 01:43:45 +0300

    BLOG_APP_COMMIT #9: Create update-functionality

--- Building-Real-Time-REST-APIs-with-Spring-Boot-Blog-App/blog-rest-api/src/main/java/com/springboot/blogrestapi/service/PostService.java
+++ Building-Real-Time-REST-APIs-with-Spring-Boot-Blog-App/blog-rest-api/src/main/java/com/springboot/blogrestapi/service/PostService.java
@@ -2,7 +2,15 @@ package com.springboot.blogrestapi.service;
 
 import com.springboot.blogrestapi.playload.PostDto;
 
+import java.util.List;
+
 public interface PostService {
 
     PostDto createPost(PostDto postDto);
+
+    List<PostDto> getAllPosts();
+
+    PostDto getPostById(long id);
+
+    PostDto updatePost(PostDto postDto, long id);
 }
--- Building-Real-Time-REST-APIs-with-Spring-Boot-Blog-App/blog-rest-api/src/main/java/com/springboot/blogrestapi/service/impl/PostServiceImpl.java
+++ Building-Real-Time-REST-APIs-with-Spring-Boot-Blog-App/blog-rest-api/src/main/java/com/springboot/blogrestapi/service/impl/PostServiceImpl.java
@@ -1,14 +1,17 @@
 package com.springboot.blogrestapi.service.impl;
 
 import com.springboot.blogrestapi.entity.Post;
+import com.springboot.blogrestapi.exception.ResourceNotFoundException;
 import com.springboot.blogrestapi.playload.PostDto;
 import com.springboot.blogrestapi.repository.PostRepository;
 import com.springboot.blogrestapi.service.PostService;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -19,15 +22,51 @@ public class PostServiceImpl implements PostService {
     @Override
     public PostDto createPost(PostDto postDto) {
         /* Convert DTO to entity */
-        Post post = new Post();
-        BeanUtils.copyProperties(postDto, post);
-
+        Post post = mapToEntity(postDto);
         Post newPost = postRepository.save(post);
-
         /* Convert entity to DTO */
-        PostDto postResponse = new PostDto();
-        BeanUtils.copyProperties(newPost, postResponse);
+        PostDto postResponse = mapToDto(newPost);
 
         return postResponse;
+    }
+
+    @Override
+    public List<PostDto> getAllPosts() {
+        List<Post> posts = postRepository.findAll();
+
+        return posts.stream()
+                .map(post -> mapToDto(post))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public PostDto getPostById(long id) {
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(Post.class.getName(),"id", id));
+
+        return mapToDto(post);
+    }
+
+    @Override
+    public PostDto updatePost(PostDto postDto, long id) {
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(Post.class.getName(),"id", id));
+
+        BeanUtils.copyProperties(postDto, post, new String[] { "id" });
+        Post updatedPost = postRepository.save(post);
+
+        return mapToDto(updatedPost);
+    }
+
+    private PostDto mapToDto(Post post) {
+        PostDto postDto = new PostDto();
+        BeanUtils.copyProperties(post, postDto);
+        return postDto;
+    }
+
+    private Post mapToEntity(PostDto postDto) {
+        Post post = new Post();
+        BeanUtils.copyProperties(postDto, post, new String[] { "id" });
+        return post;
     }
 }
