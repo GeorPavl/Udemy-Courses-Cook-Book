74d9a4afba8e51e832cef366c04c489b72f3c131
Giorgos Pavloglou <georpavloglou@gmail.com>
2022-11-15 16:06:40 +0200

    AMC #4.3: IMPLEMENT exercise for databinding

--- Angular - The Complete Guide (2023 Edition)/section-2/basics-components/src/app/servers/servers.component.ts
+++ Angular - The Complete Guide (2023 Edition)/section-2/basics-components/src/app/servers/servers.component.ts
@@ -12,6 +12,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created.';
   serverName = '';
+  username = '';
 
   constructor() { 
     setTimeout(() => {
