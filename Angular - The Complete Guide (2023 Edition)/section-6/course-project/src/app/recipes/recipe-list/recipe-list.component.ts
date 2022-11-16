e681303e8a40f4c5da902fc4307bdd9333a6fda8
Giorgos Pavloglou <georpavloglou@gmail.com>
2022-11-16 20:12:01 +0200

    AMC #6.1: IMPLEMENT components and databinding in course project

---
+++ Angular - The Complete Guide (2023 Edition)/section-6/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Recipe } from '../recipe.module';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a siple test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'),
+    new Recipe('Something', 'Be Quite!', 'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/101ad363237205.5aa9feaddf1f7.gif')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
+
+}
